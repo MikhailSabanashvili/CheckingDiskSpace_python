50acbf57f123fbc5ab26f8e6326e2c5ff63f0483
MikhailSabanashvili <47075973+MikhailSabanashvili@users.noreply.github.com>
2020-04-14 16:44:21 +0300

    uhsdfhj

--- src/Check.py
+++ src/Check.py
@@ -7,9 +7,9 @@ free_C = psutil.disk_usage(DISK_C).free / (1024 * 1024 * 1024)
 print(f"{free_C:.4} Gb free on disk {DISK_C}")
 free_E = psutil.disk_usage(DISK_E).free/(1024*1024*1024)
 print(f"{free_E:.4} Gb free on disk {DISK_E}")
-if free_E<100 and free_C<100: raise UserWarning("Место на дисках С и Е заканчивается")
-if free_C<100: raise UserWarning("Место на диске С заканчивается")
-if free_E<100: raise UserWarning("Место на диске E заканчивается")
+if free_E<5 and free_C<5: raise UserWarning("Место на дисках С и Е заканчивается")
+if free_C<5: raise UserWarning("Место на диске С заканчивается")
+if free_E<5: raise UserWarning("Место на диске E заканчивается")
 
 
 
